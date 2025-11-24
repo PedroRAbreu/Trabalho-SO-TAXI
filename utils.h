8a250689022cd38f94540f521afe19d6539900a7
leopsilva1 <74454073+leopsilva1@users.noreply.github.com>
2025-11-24 23:08:04 +0000

    adição do utils.h

---
+++ utils.h
@@ -0,0 +1,13 @@
+//
+// Created by leops on 24/11/2025.
+//
+
+#ifndef TRABALHO_SO_TAXI_UTILS_H
+#define TRABALHO_SO_TAXI_UTILS_H
+
+/*
+ * Este header vai servir para colocar-mos as estruturas e os includes.
+ * Depois e so chamar nos ficheiros.
+ * */
+
+#endif //TRABALHO_SO_TAXI_UTILS_H
